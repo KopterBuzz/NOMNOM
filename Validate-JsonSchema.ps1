7a11ff441dd19bc47f788a8d4e6e1f6f95c6a1de
Gabor Nemeth <etgabo@gmail.com>
2026-01-27 20:10:49 +0000

    adding exit codes

--- Validate-JsonSchema.ps1
+++ Validate-JsonSchema.ps1
@@ -13,12 +13,15 @@ try {
     $result = Test-Json -Json $json -Schema $schema -ErrorAction Stop
     if ($result) {
         Write-Host "JSON SCHEMA is valid." -ForegroundColor Green
+        Exit 0
     }
     else {
         Write-Host "JSON SCHEMA is invalid!" -ForegroundColor Red
+        Exit 1
     }
 }
 catch {
     Write-Host "Validation failed:" -ForegroundColor Red
     Write-Host $_
+    Exit 1
 }
